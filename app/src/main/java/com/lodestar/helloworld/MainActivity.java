1a2bd09c2da2008f3fa8a781785f831db9701f9c
Lodestar <lodestar_lin@hotmail.com>
2015-11-25 23:44:21 +0800

    test

--- app/src/main/java/com/lodestar/helloworld/MainActivity.java
+++ app/src/main/java/com/lodestar/helloworld/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {test
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
